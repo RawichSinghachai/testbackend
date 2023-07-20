a679c9a49ca95095cd5fb7349ab1c0f3fa7f165d
RawichSinghachai <107625626+RawichSinghachai@users.noreply.github.com>
2023-07-20 22:13:47 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
+
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGODB);
+
+
+
+
 const app = express();
+const port = process.env.PORT || 4000
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send('This is http get')
-})
 
-app.post('/test', (req, res) => {
-  const {name,id} = req.body
-  console.log(`name : ${name}`);
-  console.log(`id : ${id}`);
-//   console.log(req.body);
-  res.send('Received the data successfully.');
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.listen(3001, () => console.log('Running on port 3001'))
+
+app.post('/update',async(req,res)=>{
+    const {id,height,weight,datetocheck} = req.body
+        const objectId = new ObjectId(id);
+        await client.connect();
+        const users = await client
+          .db("nurse")
+          .collection("users")
+          .updateOne({_id:objectId},{$push:{height:height,weight:weight,datetocheck:datetocheck}})
+        await client.close();
+        res.json(users);
+})
